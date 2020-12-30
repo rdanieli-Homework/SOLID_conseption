d30e2e74a995d0da2efa63fea26bced9fc78721f
rodrigomdanieli <47753921+rodrigomdanieli@users.noreply.github.com>
2020-12-30 15:02:09 -0300

    Up dev

---
+++ src/Principles/02-Open_Closed/code/Problem.php
@@ -0,0 +1,44 @@
+<?php
+
+use Support\MessageInterface;
+use Support\Payment;
+
+/**
+ * Open Closed
+ * 
+ * 
+ * 
+ */
+class User
+{
+    private string $name;
+    private Array $processed_payment;
+    
+    /**
+     * This method formated the configured name 
+     */
+    public function formatName(): string
+    {
+        return ucfirst($this->name);
+    }
+
+    /**
+     * This method send a notification to user
+     */
+    public function sendReport(MessageInterface $message): void
+    {}
+
+    /**
+     * This method recive a payment of anothe user, and add to process payments
+     */
+    public function recivePaymentOfUser(Payment $user_payment): bool
+    {
+        if(!$user_payment->processPayment())
+            return false;
+
+        array_push($this->processed_payment,$user_payment);
+        return true;
+        
+    }
+
+}
